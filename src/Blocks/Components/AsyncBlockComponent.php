c8cbd01248f7a825bad438f2856a7bd3657a27d3
Zwuiix <71025924+Zwuiix-cmd@users.noreply.github.com>
2025-10-19 16:35:15 +0200

    Add AsyncBlockComponent and extend AsyncRegisterBlocksTask

---
+++ src/Blocks/Components/AsyncBlockComponent.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Nexly\Blocks\Components;
+
+use pocketmine\nbt\tag\Tag;
+use pocketmine\network\mcpe\protocol\serializer\NetworkNbtSerializer;
+
+class AsyncBlockComponent extends BlockComponent
+{
+    private Tag $nbt;
+
+    public function __construct(
+        private string $name,
+        string $nbt,
+    ) {
+        $this->nbt = (new NetworkNbtSerializer())->read(base64_decode($nbt))->getTag();
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return Tag
+     */
+    public function toNBT(): Tag
+    {
+        return $this->nbt;
+    }
+}
--- src/Tasks/AsyncRegisterBlocksTask.php
+++ src/Tasks/AsyncRegisterBlocksTask.php
@@ -4,7 +4,10 @@ namespace Nexly\Tasks;
 
 use Engine\Blocks\SchemaManager;
 use Nexly\Blocks\BlockPalette;
+use Nexly\Blocks\Components\AsyncBlockComponent;
 use Nexly\Blocks\NexlyBlocks;
+use Nexly\Blocks\Permutations\BlockProperty;
+use Nexly\Blocks\Permutations\Permutation;
 use pmmp\thread\ThreadSafeArray;
 use pocketmine\block\Block;
 use pocketmine\data\bedrock\block\convert\BlockStateReader;
@@ -13,7 +16,11 @@ use pocketmine\scheduler\AsyncTask;
 
 final class AsyncRegisterBlocksTask extends AsyncTask
 {
-    private ThreadSafeArray $blockFuncs;
+    private ThreadSafeArray $numericIds;
+    private ThreadSafeArray $func;
+    private ThreadSafeArray $properties;
+    private ThreadSafeArray $permutations;
+    private ThreadSafeArray $components;
     private ThreadSafeArray $serializer;
     private ThreadSafeArray $deserializer;
 
@@ -23,26 +30,50 @@ final class AsyncRegisterBlocksTask extends AsyncTask
      */
     public function __construct(array $blocks)
     {
-        $this->blockFuncs = new ThreadSafeArray();
+        $this->numericIds = new ThreadSafeArray();
+        $this->func = new ThreadSafeArray();
+        $this->properties = new ThreadSafeArray();
+        $this->permutations = new ThreadSafeArray();
+        $this->components = new ThreadSafeArray();
         $this->serializer = new ThreadSafeArray();
         $this->deserializer = new ThreadSafeArray();
 
-        foreach ($blocks as $identifier => [$blockFunc, $serializer, $deserializer]) {
-            $this->blockFuncs[$identifier] = $blockFunc;
-            $this->serializer[$identifier] = $serializer;
-            $this->deserializer[$identifier] = $deserializer;
+        foreach ($blocks as $stringId => [$numericId, $func, $properties, $permutations, $components, $serializer, $deserializer]) {
+            $this->numericIds[$stringId] = $numericId;
+            $this->func[$stringId] = $func;
+            $this->properties[$stringId] = $properties;
+            $this->permutations[$stringId] = $permutations;
+            $this->components[$stringId] = $components;
+            $this->serializer[$stringId] = $serializer;
+            $this->deserializer[$stringId] = $deserializer;
         }
     }
 
     public function onRun(): void
     {
-        foreach ($this->blockFuncs as $identifier => $blockFunc) {
+        foreach ($this->func as $stringId => $blockFunc) {
             // We do not care about the model or creative inventory data in other threads since it is unused outside of
             // the main thread.
-            NexlyBlocks::buildComplex($identifier, $blockFunc)
-                ->setSerializer($this->serializer[$identifier])
-                ->setDeserializer($this->deserializer[$identifier])
-            ->register(false);
+            $builder = NexlyBlocks::buildComplex($stringId, $blockFunc)
+                ->setNumericId($this->numericIds[$stringId])
+                ->setSerializer($this->serializer[$stringId])
+                ->setDeserializer($this->deserializer[$stringId]);
+
+            foreach (json_decode($this->properties[$stringId], true) as $property) {
+                $builder->addProperty(new BlockProperty($property["name"], $property["values"]));
+            }
+            foreach (json_decode($this->permutations[$stringId], true) as $permutation) {
+                $p = Permutation::create($permutation["condition"]);
+                foreach ($permutation["components"] as $component) {
+                    $p->addComponent(new AsyncBlockComponent($component["name"], $component["nbt"]));
+                }
+                $builder->addPermutation($p);
+            }
+            foreach (json_decode($this->components[$stringId], true) as $component) {
+                $builder->addComponent(new AsyncBlockComponent($component["name"], $component["nbt"]));
+            }
+
+            $builder->register(false, false);
         }
 
         // Sort the block palette
